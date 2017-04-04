e15134e303a927b0157ebe57aaad07499296774d
vinod-sakhatech <vgmadiwalar1993@gmail.com>
2017-04-04 17:56:32 +0530

    code base

---
+++ src/main/java/com/pustaka/dto/Book.java
@@ -0,0 +1,50 @@
+package com.pustaka.dto;
+
+public class Book 
+{
+	private int bookId;
+	private String title;
+	private String desc;
+	private int edition;
+	private double price;
+	private long userId;
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public int getEdition() {
+		return edition;
+	}
+	public void setEdition(int edition) {
+		this.edition = edition;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	
+	
+
+}
